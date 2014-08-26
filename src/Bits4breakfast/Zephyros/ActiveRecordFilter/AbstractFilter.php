e2a8175aede8bcf7275df9174d9a37965d51efae
Matteo Galli <thinkindie@me.com>
2014-08-27 00:35:37 +0200

    refactoring

--- src/Bits4breakfast/Zephyros/ActiveRecordFilter/AbstractFilter.php
+++ src/Bits4breakfast/Zephyros/ActiveRecordFilter/AbstractFilter.php
@@ -6,4 +6,12 @@ abstract class AbstractFilter {
 	const CODE = 0;
 
 	public $error_string = self::NAME;
+
+	public function __construct($error_string = '') {
+		if ($error_string == '') {
+			$this->error_string = self::CODE;
+		} else {
+			$this->error_string = $error_string;
+		}
+	}
 }
--- src/Bits4breakfast/Zephyros/ActiveRecordFilter/ValidateUnique.php
+++ src/Bits4breakfast/Zephyros/ActiveRecordFilter/ValidateUnique.php
@@ -4,10 +4,4 @@ namespace Bits4breakfast\Zephyros\ActiveRecordFilter;
 class ValidateUnique extends AbstractFilter {
 	const NAME = 'UNIQUE';
 	const CODE = '';
-
-	public $error_string = '';
-
-	public function __construct($error_string) {
-		$this->error_string = $error_string;
-	}
 }
