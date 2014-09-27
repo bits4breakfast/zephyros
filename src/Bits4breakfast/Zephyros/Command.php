1d844704fb43e48a72a1b286c04bee118b68106c
Matteo Galli <thinkindie@mac.com>
2014-09-27 12:13:48 +0200

    base command

---
+++ src/Bits4breakfast/Zephyros/Command.php
@@ -0,0 +1,26 @@
+<?php
+namespace Bits4breakfast\Zephyros;
+
+use Symfony\Component\Console\Command\Command as BaseCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Command extends BaseCommand
+{
+	protected $environemnt = 'dev';
+	protected $app_base_path;
+
+	protected $config = null;
+	protected $container = null;
+
+	private function initialize(InputInterface $input, OutputInterface $output) 
+	{
+		$this->environemnt = $input->getParameterOption('env', 'dev');
+		$this->app_base_path = realpath(getcwd().'/../../..');
+
+		$this->config = new Config($this->app_base_path, 'console-commands', $this->environemnt);
+
+		$services = new ServiceContainerDefinitions($this->app_base_path);
+		$this->container = ServiceContainer::init($this->config, $services);
+	}
+}
