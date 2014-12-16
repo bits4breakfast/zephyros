7240120c90b7eb160de2c6aad4e2c1aa160d2973
Matteo Galli <thinkindie@me.com>
2014-12-17 00:57:10 +0100

    fixes zephyros

--- src/Bits4breakfast/Zephyros/ServiceContainerDefinitionsInterface.php
+++ src/Bits4breakfast/Zephyros/ServiceContainerDefinitionsInterface.php
@@ -3,11 +3,11 @@ namespace Bits4breakfast\Zephyros;
 
 interface ServiceContainerDefinitionsInterface {
 
-	public function __construct($app_base_path) {}
+	public function __construct($app_base_path);
 
-	public function load() {}
+	public function load();
 
-	public function get($key) {}
+	public function get($key);
 
-	public function dump() {}
+	public function dump();
 }
