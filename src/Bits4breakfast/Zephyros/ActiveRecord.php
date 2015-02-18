4a6e15cd133e4a3547b6f14607dac08d24105f2b
Matteo Galli <thinkindie@mac.com>
2015-02-19 00:10:13 +0100

    fixes a notice

--- src/Bits4breakfast/Zephyros/ActiveRecord.php
+++ src/Bits4breakfast/Zephyros/ActiveRecord.php
@@ -583,7 +583,15 @@ abstract class ActiveRecord {
 			
 			$this->_db->pick($this->_shard)->upsert( $this->_table, $this->_data, (isset($this->columns_to_increment)?$this->columns_to_increment:null) );
 			
-			if ($this->_identifier == 'id' && $this->_data['id'] == 0) {
+			if (
+				$this->_identifier == 'id' && 
+				(
+					!isset($this->_data['id']) || 
+					(
+						isset($this->_data['id']) &&
+						$this->_data['id'] == 0
+					)
+				) {
 				$this->_data['id'] = (int) $this->_db->pick($this->_shard)->last_id();
 				if ($this->_data['id'] == 0) {
 					throw new PersistingErrorException();
