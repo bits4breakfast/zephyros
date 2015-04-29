32eacfaf087acee2e8e5d11a94b6cb6bd57fbc5c
Matteo Galli <thinkindie@mac.com>
2015-04-29 09:41:15 +0200

    code style

--- src/Bits4breakfast/Zephyros/Router.php
+++ src/Bits4breakfast/Zephyros/Router.php
@@ -23,13 +23,13 @@ class Router {
 			throw new BadRequestException();
 		}
 
-		if ( isset($_GET['controller']) ) {
+		if (isset($_GET['controller'])) {
 			$this->route->controller = $_GET['controller'];
 			$this->route->action = isset($_GET['action']) ? $_GET['action'] : '';
 			$this->route->id = isset($_GET['id']) ? $_GET['id'] : 0;
 		} else {
 			$uri = substr($_SERVER["REQUEST_URI"], 1);
-			if ( strpos($uri, "?") !== false ) {
+			if (strpos($uri, "?") !== false) {
 				$uri = explode("?", $uri);
 				$uri = $uri[0];
 			}
@@ -37,29 +37,29 @@ class Router {
 			list($this->route->controller, $this->route->action, $this->route->id) = (array) array_pad(explode("/", $uri), 3, "");
 		}
 
-		foreach ( ['json', 'csv'] as $format ) {
-			if ( strpos($this->route->id, ".".$format) !== false ) {
+		foreach (['json', 'csv'] as $format) {
+			if (strpos($this->route->id, ".".$format) !== false) {
 				$this->route->format = $format;
 				$this->route->id = str_replace(".".$format, "", $this->route->id);
 				break;
-			} else if ( strpos($this->route->controller, ".".$format) !== false ) {
+			} else if (strpos($this->route->controller, ".".$format) !== false) {
 				$this->route->format = $format;
 				$this->route->controller = str_replace(".".$format, "", $this->route->controller);
 				break;
-			} else if ( strpos($this->route->action, ".".$format) !== false ) {
+			} else if (strpos($this->route->action, ".".$format) !== false) {
 				$this->route->format = $format;
 				$this->route->action = str_replace(".".$format, "", $this->route->action);
 				break;
 			}
 		}
 
-		if ( is_numeric($this->route->action) ) {
+		if (is_numeric($this->route->action)) {
 			$this->route->id = (int) $this->route->action;
 			$this->route->action = "";
 		}
 
-		if ( $this->route->id != null ) {
-			if ( is_numeric($this->route->id) ) {
+		if ($this->route->id != null) {
+			if (is_numeric($this->route->id)) {
 				$this->route->id = (int) $this->route->id;
 			} else {
 				$this->route->id = @mysql_escape_string($this->route->id);
@@ -68,7 +68,7 @@ class Router {
 			$this->route->id = 0;
 		}
 
-		if ( $this->route->controller == "" ) {
+		if ($this->route->controller == "") {
 			$this->route->controller = "home";
 		}
 		
