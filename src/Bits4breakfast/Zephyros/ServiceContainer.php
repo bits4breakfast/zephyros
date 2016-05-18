ea5c699da0c63e59a6bd6e07ee2966d09c157ab7
Matteo Galli <thinkindie@mac.com>
2016-05-18 22:59:53 +0200

    Improve logging

--- src/Bits4breakfast/Zephyros/ServiceContainer.php
+++ src/Bits4breakfast/Zephyros/ServiceContainer.php
@@ -5,6 +5,7 @@ use Bits4breakfast\Zephyros\ConfigInterface;
 use Bits4breakfast\Zephyros\ServiceContainer;
 use Bits4breakfast\Zephyros\ServiceContainerDefinitionsInterface;
 
+use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use Bits4breakfast\Zephyros\Service\Mysql;
 use Bits4breakfast\Zephyros\Service\Cache;
@@ -13,140 +14,153 @@ use Bits4breakfast\Zephyros\Service\MessageBus;
 
 class ServiceContainer {
 
-	private static $instance = null;
-	
-	private $service_container_definitions = null;
-	private $services = [];
+    private static $instance = null;
 
-	public static function init(ConfigInterface $config = null, ServiceContainerDefinitionsInterface $service_container_definitions = null) {
-		if ($config == null && self::$instance == null) {
-			throw new \InvalidArgumentException('config instance cannot be a null reference');
-		}
+    private $service_container_definitions = null;
+    private $services = [];
 
-		if ($service_container_definitions == null && self::$instance == null) {
-			throw new \InvalidArgumentException('service_container_definitions instance cannot be a null reference');
-		}
+    public static function init(ConfigInterface $config = null, ServiceContainerDefinitionsInterface $service_container_definitions = null) {
+        if ($config == null && self::$instance == null) {
+            throw new \InvalidArgumentException('config instance cannot be a null reference');
+        }
 
-		if (self::$instance === null) {
-			self::$instance = new ServiceContainer($config, $service_container_definitions);
-		}
+        if ($service_container_definitions == null && self::$instance == null) {
+            throw new \InvalidArgumentException('service_container_definitions instance cannot be a null reference');
+        }
 
-		return self::$instance;
-	}
+        if (self::$instance === null) {
+            self::$instance = new ServiceContainer($config, $service_container_definitions);
+        }
 
-	private function __construct(ConfigInterface $config, ServiceContainerDefinitionsInterface $service_container_definitions) {
-		$this->service_container_definitions = $service_container_definitions;
+        return self::$instance;
+    }
 
-		$this->register('bits4brekfast.zephyros.config', $config);
+    private function __construct(
+        ConfigInterface $config,
+        ServiceContainerDefinitionsInterface $service_container_definitions
+    ) {
+        $this->service_container_definitions = $service_container_definitions;
 
-		$logger = new Logger($config->get('kernel_app_id').$config->subdomain);
-		if (!empty($config->get('log.dir_path'))) {
-			$logger->pushHandler(
-				new \Monolog\Handler\StreamHandler(
-					$config->get('log.dir_path').'/'.@date('Y-m-d').'.log', 
-					($config->get('log.level') ? $config->get('log.level') : 500),
-					true,
-					0777
-				)
-			);
-		}
+        $this->register('bits4brekfast.zephyros.config', $config);
 
-		$this->register('bits4brekfast.zephyros.logger', $logger);
-		
-		$this->register('bits4brekfast.zephyros.cache', new Cache($this));
-		$this->register('bits4brekfast.zephyros.message_bus', new MessageBus($this));
-	}
+        $logger = new Logger($config->get('kernel_app_id').$config->subdomain);
+        if (!empty($config->get('log.dir_path'))) {
+            $fileName = $config->get('log.dir_path') . '/' . @date('Y-m-d') . '.log';
+            $filePointer = fopen($fileName, 'a');
+            @chmod($fileName, 0777);
 
-	public function register($service_id, $instance) {
-		if (empty($service_id)) {
-			throw new \InvalidArgumentException('service_id cannot be an empty string');
-		}
+            $logger->pushHandler(
+                new StreamHandler(
+                    $filePointer,
+                    ($config->get('log.level') ? $config->get('log.level') : 500),
+                    true
+                )
+            );
+        } else {
+            $logger->pushHandler(
+                new StreamHandler(
+                    'php://stderr',
+                    ($config->get('log.level') ? $config->get('log.level') : 500)
+                )
+            );
+        }
 
-		if ($instance == null) {
-			throw new \InvalidArgumentException('instance cannot be a null reference');
-		}
+        $this->register('bits4brekfast.zephyros.logger', $logger);
 
-		if (isset($this->services[$service_id])) {
-			throw new \InvalidArgumentException('you cannot replace '.$service_id.' with another instance');	
-		}
+        $this->register('bits4brekfast.zephyros.cache', new Cache($this));
+        $this->register('bits4brekfast.zephyros.message_bus', new MessageBus($this));
+    }
 
-		$this->services[$service_id] = $instance;
+    public function register($service_id, $instance) {
+        if (empty($service_id)) {
+            throw new \InvalidArgumentException('service_id cannot be an empty string');
+        }
 
-		return $this;
-	}
+        if ($instance == null) {
+            throw new \InvalidArgumentException('instance cannot be a null reference');
+        }
 
-	public function get($service_id) {
-		if (empty($service_id)) {
-			throw new \InvalidArgumentException('service_id cannot be an empty string');
-		}
+        if (isset($this->services[$service_id])) {
+            throw new \InvalidArgumentException('you cannot replace '.$service_id.' with another instance');
+        }
 
-		if (isset($this->services[$service_id])) {
-			return $this->services[$service_id];
-		}
+        $this->services[$service_id] = $instance;
 
-		switch ($service_id) {
-			case 'logger':
-				return $this->logger();
-			case 'cache':
-				return $this->cache();
-			case 'config':
-				return $this->config();
-			case 'lm':
-				return $this->lm();
-			case 'db':
-				return $this->db();
-			case 'mailer':
-				return $this->mailer();
-		}
+        return $this;
+    }
 
-		if ($class_name = $this->service_container_definitions->get($service_id)) {
-			if (in_array('Bits4breakfast\\Zephyros\\ServiceInterface', class_implements($class_name))) {
-				$instance = new $class_name($this);
-				$this->register($service_id, $instance);
+    public function get($service_id) {
+        if (empty($service_id)) {
+            throw new \InvalidArgumentException('service_id cannot be an empty string');
+        }
 
-				return $instance;
-			}
+        if (isset($this->services[$service_id])) {
+            return $this->services[$service_id];
+        }
 
-			throw new \RuntimeException($class_name.' must implement Bits4breakfast\\Zephyros\\ServiceInterface');
-		}
+        switch ($service_id) {
+            case 'logger':
+                return $this->logger();
+            case 'cache':
+                return $this->cache();
+            case 'config':
+                return $this->config();
+            case 'lm':
+                return $this->lm();
+            case 'db':
+                return $this->db();
+            case 'mailer':
+                return $this->mailer();
+        }
 
-		throw new \OutOfRangeException('$service_id was not found');
-	}
+        if ($class_name = $this->service_container_definitions->get($service_id)) {
+            if (in_array('Bits4breakfast\\Zephyros\\ServiceInterface', class_implements($class_name))) {
+                $instance = new $class_name($this);
+                $this->register($service_id, $instance);
 
-	public function logger() {
-		return $this->services['bits4brekfast.zephyros.logger'];
-	}
+                return $instance;
+            }
 
-	public function cache() {
-		return $this->services['bits4brekfast.zephyros.cache'];
-	}
+            throw new \RuntimeException($class_name.' must implement Bits4breakfast\\Zephyros\\ServiceInterface');
+        }
 
-	public function config() {
-		return $this->services['bits4brekfast.zephyros.config'];
-	}
+        throw new \OutOfRangeException('$service_id was not found');
+    }
 
-	public function lm() {
-		if (!isset($this->services['bits4brekfast.zephyros.lm'])) {
-			$this->register('bits4brekfast.zephyros.lm', new LanguageManager($this));
-		}
-		return $this->services['bits4brekfast.zephyros.lm'];
-	}
+    public function logger() {
+        return $this->services['bits4brekfast.zephyros.logger'];
+    }
 
-	public function db() {
-		if (!isset($this->services['bits4brekfast.zephyros.db'])) {
-			$this->register('bits4brekfast.zephyros.db', new Mysql($this));
-		}
-		return $this->services['bits4brekfast.zephyros.db'];
-	}
+    public function cache() {
+        return $this->services['bits4brekfast.zephyros.cache'];
+    }
 
-	public function bus() {
-		return $this->services['bits4brekfast.zephyros.message_bus'];
-	}
+    public function config() {
+        return $this->services['bits4brekfast.zephyros.config'];
+    }
 
-	public function mailer() {
-		if (!isset($this->services['bits4brekfast.zephyros.mailer'])) {
-			$this->register('bits4brekfast.zephyros.mailer', new Service\Mailer($this));
-		}
-		return $this->services['bits4brekfast.zephyros.mailer'];
-	}
+    public function lm() {
+        if (!isset($this->services['bits4brekfast.zephyros.lm'])) {
+            $this->register('bits4brekfast.zephyros.lm', new LanguageManager($this));
+        }
+        return $this->services['bits4brekfast.zephyros.lm'];
+    }
+
+    public function db() {
+        if (!isset($this->services['bits4brekfast.zephyros.db'])) {
+            $this->register('bits4brekfast.zephyros.db', new Mysql($this));
+        }
+        return $this->services['bits4brekfast.zephyros.db'];
+    }
+
+    public function bus() {
+        return $this->services['bits4brekfast.zephyros.message_bus'];
+    }
+
+    public function mailer() {
+        if (!isset($this->services['bits4brekfast.zephyros.mailer'])) {
+            $this->register('bits4brekfast.zephyros.mailer', new Service\Mailer($this));
+        }
+        return $this->services['bits4brekfast.zephyros.mailer'];
+    }
 }
